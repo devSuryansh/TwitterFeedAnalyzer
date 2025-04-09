9232bec6273b50809c56b9a966ad2dbd8174e69c
devSuryansh <suryanshg2050@gmail.com>
2025-04-09 10:44:51 +0530

    Sets up analysis

---
+++ analysis/sentiment.py
@@ -0,0 +1,11 @@
+from textblob import TextBlob
+
+def analyze_sentiment(text):
+    analysis = TextBlob(text)
+    polarity = analysis.sentiment.polarity
+    if polarity > 0:
+        return "Positive"
+    elif polarity == 0:
+        return "Neutral"
+    else:
+        return "Negative"
